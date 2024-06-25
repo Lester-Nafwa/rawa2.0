d993efffb1ac7d8ef55478e62dad3c2cfea4f5e8
Lester-Nafwa <nafwalester@gmail.com>
2024-06-26 02:54:22 +0300

    formating

--- src/App.js
+++ src/App.js
@@ -1,47 +1,32 @@
- // App.js
 import "./App.css";
 import About from "./component/About/About";
 import PartnerWithUs from "./component/PartnerUs/PartnerUs";
 import DashboardLayout from "./component/DashbordLayout/Dashboard";
 import DynamicSectors from "./component/Sectors/DynamicSectors";
-import {
-  Route,
-  RouterProvider,
-  createRoutesFromElements,
-  createBrowserRouter,
-  Navigate
-} from "react-router-dom";
+import { Route, RouterProvider, createRoutesFromElements, createBrowserRouter, Navigate } from "react-router-dom";
 import DesignLayout from "./Layouts/DesignLayout";
 import ContactUs from "./component/ContactUs/ContactUs";
 import Sectors from "./component/Sectors/Sectors";
-// import SectorList from "./component/Sectors/SectorList";
 
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <>
-        <Route path="/" replace element={<DesignLayout />}>
-        <Route path="/" element={<Navigate to="/home" replace />} />
+        <Route path="/" element={<DesignLayout />}>
+          <Route path="/" element={<Navigate to="/home" replace />} />
           <Route path="about" element={<About />} />
           <Route path="contacts" element={<ContactUs />} />
           <Route path="partnerships" element={<PartnerWithUs />} />
           <Route path="sectors" element={<Sectors />} />
           <Route path="sectors/:id" element={<DynamicSectors />} />
           <Route path="/our-work" element={<DynamicSectors />} />
-          <Route path="/our-work:id" element={<DynamicSectors />} />
-   
+          <Route path="/our-work/:id" element={<DynamicSectors />} />
         </Route>
         <Route path="/home" element={<DashboardLayout />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contacts" element={<ContactUs />} />
-        <Route path="/partnerships" element={<PartnerWithUs />} />
-        <Route path="/sectors" element={<Sectors />} />
-        <Route path="/sectors/:id" element={<DynamicSectors />} />
-        <Route path="/our-work:id" element={<DynamicSectors />} />
-        <Route path="/our-work" element={<DynamicSectors />} />
       </>
     )
   );
+
   return <RouterProvider router={router} />;
 }
 
