1548bbf27e34aee48852c934fe6e9daae273c9a7
Lester Nafwa <lester.nafwa@MDGF018F44.local>
2025-02-03 11:46:25 +0300

    fix nav

---
+++ src/Utils/About.jsx
@@ -0,0 +1,56 @@
+export const aboutData = [
+    {
+      title: "Who We Are",
+      Image: "/who_we_are.jpeg",
+      content: `
+        Established in 2005, Ras-Awad Welfare Association (RAWA) is a
+        non-profit organization dedicated at improving community well-being
+        through health, education, and advocacy. Focusing on WASH projects,
+        capacity building, and education, RAWA supports underserved
+        populations, including marginalized groups like pastoralists and
+        girls. RAWA enhance educational access, provide vocational training,
+        and run public health campaigns. RAWA’s evidence-based approach uses
+        comprehensive research and continuous evaluation to ensure effective
+        interventions and lasting community impact.
+      `
+    },
+    {
+      title: "Objective",
+      content: `
+        Improve Community Resilience: Setting up support systems and
+          rallying resources to aid those in need
+        Promote Self-Reliance: Honing skills, providing vocational
+          training, and offering entrepreneurial guidance to pave the way
+          for self-sufficiency
+       Strengthen Local Capacities: Empower local government bodies,
+          community entities, and organizations to tackle and oversee
+          development needs for youths and women, involve them in health,
+          education, environment, and economic activities, and enhance
+          access to information on youth and social development for
+          policymakers and practitioners
+      `,
+      Image: "/objectives_lngo.jpg",
+
+    },
+    {
+      title: "Mission",
+      Image: "/our_mission.jpg",
+      content: `
+        To enhance the living standards of Somali communities by
+        fostering self-reliance and prosperity. We achieve this through
+        the provision of relief, rehabilitation, and development
+        programs, and by strengthening local capacities to address their
+        needs in a transparent and participatory manner.
+      `
+    },
+    {
+      title: "Vision",
+      Image: "/our_vision.png",
+      content: `
+        A prosperous and self-reliant Somali society, where empowered
+        communities effectively address their challenges and achieve
+        sustainable development.
+      `
+    }
+  ];
+  
--- src/component/About/About.jsx
+++ src/component/About/About.jsx
@@ -1,72 +1,51 @@
 import "./about.css";
+import { aboutData } from "../../Utils/About";
+import { useState } from "react";
 
 const About = () => {
+  // Default to "Who We Are" (index 0)
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
   return (
     <div className="about_us_container">
-      <div className="about_us">
-        <div className="about">
-          <span className="about_title">Who We Are</span>
-        </div>
-        <p className="about_description">
-          Established in 2005, Ras-Awad Welfare Association (RAWA) is a
-          non-profit organization dedicated at improving community well-being
-          through health, education, and advocacy. Focusing on WASH projects,
-          capacity building, and education, RAWA supports underserved
-          populations, including marginalized groups like pastoralists and
-          girls. RAWA enhance educational access, provide vocational training,
-          and run public health campaigns. RAWA’s evidence-based approach uses
-          comprehensive research and continuous evaluation to ensure effective
-          interventions and lasting community impact.
-        </p>
-        <div>
-          <div className="objectives_sect">
-            <span>Objective</span>
-            <li>
-              Improve Community Resilience: Setting up support systems and
-              rallying resources to aid those in need
-            </li>
-            <li>
-              Promote Self-Reliance: Honing skills, providing vocational
-              training, and offering entrepreneurial guidance to pave the way
-              for self-sufficiency
-            </li>
-            <li>
-              Strengthen Local Capacities: Empower local government bodies,
-              community entities, and organizations to tackle and oversee
-              development needs for youths and women, involve them in health,
-              education, environment, and economic activities, and enhance
-              access to information on youth and social development for
-              policymakers and practitioners
-            </li>
-          </div>
-        </div>
-      </div>
-      <div className="sectors">
-        <div className="mission_vision">
-          <div className="mission">
-            <span className="mission_title">Mission</span>
-            <div>
-              <p>
-                To enhance the living standards of Somali communities by
-                fostering self-reliance and prosperity. We achieve this through
-                the provision of relief, rehabilitation, and development
-                programs, and by strengthening local capacities to address their
-                needs in a transparent and participatory manner.
-              </p>
-            </div>
-          </div>
-          <div className="vision">
-            <span>Vision</span>
-            <div>
-              <p>
-                A prosperous and self-reliant Somali society, where empowered
-                communities effectively address their challenges and achieve
-                sustainable development.
-              </p>
+      <section>
+        <nav className="about_nav">
+          <ul>
+            {aboutData.map((item, index) => (
+              <li
+                key={index}
+                className={selectedIndex === index ? "active" : ""}
+                onClick={() => setSelectedIndex(index)}
+              >
+                {item.title}
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </section>
+      <section>
+        <div className="about_us">
+          <div className="about">
+            <span className="about_title">
+              {aboutData[selectedIndex].title}
+            </span>
+            <div className="about_content_sub_container">
+              <div c >  
+                <img
+                  src={aboutData[selectedIndex].Image}
+                  alt={aboutData[selectedIndex].title}
+                  className="about_image_container"
+                />
+              </div>
+              <div className="about_description_container">
+                <p className="about_description">
+                  {aboutData[selectedIndex].content}
+                </p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </section>
     </div>
   );
 };
